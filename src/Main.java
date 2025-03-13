df4f5662048d84a2382842ac1404c8e036a47c11
Kwame pearcraft Puryear <kwame@pearcraft.com>
2025-03-13 11:09:54 -0700

    Added voteCount output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        int[][] votesTable =
+                { {12, 13, 14, 10, 15, 16}, // candidate 0
+                        { 5,  2,  6,  8,  4,  3}, // candidate 1
+                        {13, 15, 10, 12, 11, 17}, // candidate 2
+                };
+//        for(int row = 0; row < votesTable.length; row++) {
+//            int voteCount = 0;
+//            for(int column = 0; column < votesTable[row].length; column++) {
+//                voteCount += votesTable[row][column];
+//            }
+//            System.out.println("Candidate " + row + ": "
+//                    + voteCount + " votes.");
+        int voteCount = 0;
+        System.out.println("Votes Table Length: " + votesTable.length);
+        for (int row[] : votesTable) { // each row (canidate)
+            voteCount = 0;
+            for (int vote: row) {
+                voteCount += vote;
+            }
+            System.out.println("Votes: " + voteCount);
+        }
+
+       // Output:
+       // Candidate 0: 80 votes.
+       //         Candidate 1: 28 votes.
+       //         Candidate 2: 78 votes.
+
     }
 }
